3f5d77446a30b1873ecda25668a26b7cc4ae299f
zjl <913065404@qq.com>
2018-08-21 21:05:44 +0800

    time
    增加一个时间戳 每秒更新dom

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>楼船夜雪瓜洲渡 铁马秋风大散关</p>
+      <p>现在是 {new Date().toLocaleTimeString()}</p>
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+function tick() {
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
+}
+setInterval(tick, 1000);
