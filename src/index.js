f8477651c273eb88fd246524f446adc529cf4777
zjl <913065404@qq.com>
2018-08-22 16:47:34 +0800

    inline style

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>楼船夜雪瓜洲渡 铁马秋风大散关</p>
-      <p>现在是 {new Date().toLocaleTimeString()}</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <p style={{ color: "red" }}>楼船夜雪瓜洲渡 铁马秋风大散关</p>
+            <p>现在是 {new Date().toLocaleTimeString()}</p>
+        </div>
+    );
 }
 
 function tick() {
-  const rootElement = document.getElementById("root");
-  ReactDOM.render(<App />, rootElement);
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(<App />, rootElement);
 }
 setInterval(tick, 1000);
