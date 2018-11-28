a0babd7be92849a5dbd5fe64859862087d9d2777
zhangjiliang <zhangjiliang@guazi.com>
2018-11-28 14:30:09 +0800

    green

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
             <h1>Hello CodeSandbox</h1>
             <h2>Start editing to see some magic happen!</h2>
-            <p style={{ color: "red" }}>楼船夜雪瓜洲渡 铁马秋风大散关</p>
+            <p style={{ color: "green" }}>楼船夜雪瓜洲渡 铁马秋风大散关</p>
             <p>六朝旧事随流水 但寒烟衰草凝绿</p>
             <p>现在是 {new Date().toLocaleTimeString()}</p>
         </div>
